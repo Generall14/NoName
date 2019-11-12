406f82012ecf9bde171ae98fb03ae176cb966918
Wojciech Kogut <wojciech_kogut@o2.pl>
2019-11-12 22:33:06 +0100

    Code formated.

--- M3/Src/main.c
+++ M3/Src/main.c
@@ -75,93 +75,93 @@ int main(void)
 	uint32_t cntm = 0;
 	volatile uint32_t last_timestamp, current_timestamp, diff_timestamp, flash_erase_time, flash_write_time, flash_unlock_time;
 	volatile uint32_t flash_read_time, flash_read;
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
 //  MX_USB_DEVICE_Init();
-  MX_TIM2_Init();
-  /* USER CODE BEGIN 2 */
+	MX_TIM2_Init();
+	/* USER CODE BEGIN 2 */
 
 
-  //FLASH test
+	//FLASH test
 #ifdef xxxxxxxxxxxxxxxx
-  uint32_t PageError;
-  FLASH_EraseInitTypeDef ddd={FLASH_TYPEERASE_PAGES, 0, 0x801F800u, 1};
-  last_timestamp = get_global_clock();
-  HAL_FLASH_Unlock();
-  current_timestamp = get_global_clock();
-  flash_unlock_time = current_timestamp - last_timestamp;
+	uint32_t PageError;
+	FLASH_EraseInitTypeDef ddd= {FLASH_TYPEERASE_PAGES, 0, 0x801F800u, 1};
+	last_timestamp = get_global_clock();
+	HAL_FLASH_Unlock();
+	current_timestamp = get_global_clock();
+	flash_unlock_time = current_timestamp - last_timestamp;
 
-  last_timestamp = get_global_clock();
-  HAL_FLASHEx_Erase(&ddd, &PageError);
-  current_timestamp = get_global_clock();
-  flash_erase_time = current_timestamp - last_timestamp;
+	last_timestamp = get_global_clock();
+	HAL_FLASHEx_Erase(&ddd, &PageError);
+	current_timestamp = get_global_clock();
+	flash_erase_time = current_timestamp - last_timestamp;
 
-  for(uint32_t i=0;i<16;i++)
-  {
-	  last_timestamp = get_global_clock();
-	  HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, 0x801F800u+4*i, 0x11111111*i);
-	  current_timestamp = get_global_clock();
-	  flash_write_time = current_timestamp - last_timestamp;
-  }
+	for(uint32_t i=0; i<16; i++)
+	{
+		last_timestamp = get_global_clock();
+		HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, 0x801F800u+4*i, 0x11111111*i);
+		current_timestamp = get_global_clock();
+		flash_write_time = current_timestamp - last_timestamp;
+	}
 
-  HAL_FLASH_Lock();
-  HAL_FLASH_Unlock();
-  for(uint32_t i=0;i<16;i++)
-  	HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, 0x801F800u+4*i+4*16, 0x11111111*i);
-  HAL_FLASH_Lock();
+	HAL_FLASH_Lock();
+	HAL_FLASH_Unlock();
+	for(uint32_t i=0; i<16; i++)
+		HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, 0x801F800u+4*i+4*16, 0x11111111*i);
+	HAL_FLASH_Lock();
 
-  last_timestamp = get_global_clock();
-  uint32_t* ptr = 0x801F800u+4*2+4;
-  flash_read = *ptr;
-  current_timestamp = get_global_clock();
-  flash_read_time = current_timestamp - last_timestamp;
+	last_timestamp = get_global_clock();
+	uint32_t* ptr = 0x801F800u+4*2+4;
+	flash_read = *ptr;
+	current_timestamp = get_global_clock();
+	flash_read_time = current_timestamp - last_timestamp;
 #endif
 
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1)
 	{
 //		cntm++;
 //		if(cntm >= 0x1193f)
 //		{
 //			asm volatile ("cpsid i");
-			__disable_irq();
-			SWAP_BIT(GPIOB->ODR, GPIO_ODR_ODR8+cntm);
-			SET_BIT(GPIOB->ODR, GPIO_ODR_ODR9);
-			CLEAR_BIT(GPIOB->ODR, GPIO_ODR_ODR10);
+		__disable_irq();
+		SWAP_BIT(GPIOB->ODR, GPIO_ODR_ODR8+cntm);
+		SET_BIT(GPIOB->ODR, GPIO_ODR_ODR9);
+		CLEAR_BIT(GPIOB->ODR, GPIO_ODR_ODR10);
 //			asm volatile ("cpsie i");
-			__enable_irq();
-			last_timestamp = current_timestamp;
-			current_timestamp = get_global_clock();
-			diff_timestamp = current_timestamp - last_timestamp;
-			last_timestamp *= .001;
-			//raise_error(ERROR_UNDEFINED);
+		__enable_irq();
+		last_timestamp = current_timestamp;
+		current_timestamp = get_global_clock();
+		diff_timestamp = current_timestamp - last_timestamp;
+		last_timestamp *= .001;
+		//raise_error(ERROR_UNDEFINED);
 //			cntm=0;
 //		}
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
@@ -170,45 +170,45 @@ int main(void)
   */
 void SystemClock_Config(void)
 {
-  LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
+	LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
 
-   if(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_2)
-  {
-    Error_Handler();  
-  }
-  LL_RCC_HSE_Enable();
+	if(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_2)
+	{
+		Error_Handler();
+	}
+	LL_RCC_HSE_Enable();
 
-   /* Wait till HSE is ready */
-  while(LL_RCC_HSE_IsReady() != 1)
-  {
-    
-  }
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE_DIV_1, LL_RCC_PLL_MUL_9);
-  LL_RCC_PLL_Enable();
+	/* Wait till HSE is ready */
+	while(LL_RCC_HSE_IsReady() != 1)
+	{
 
-   /* Wait till PLL is ready */
-  while(LL_RCC_PLL_IsReady() != 1)
-  {
-    
-  }
-  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
-  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
-  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+	}
+	LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE_DIV_1, LL_RCC_PLL_MUL_9);
+	LL_RCC_PLL_Enable();
 
-   /* Wait till System clock is ready */
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
-  {
-  
-  }
-  LL_Init1msTick(72000000);
-  LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
-  LL_SetSystemCoreClock(72000000);
-  LL_RCC_SetUSBClockSource(LL_RCC_USB_CLKSOURCE_PLL_DIV_1_5);
+	/* Wait till PLL is ready */
+	while(LL_RCC_PLL_IsReady() != 1)
+	{
+
+	}
+	LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+	LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
+	LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+	LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+	/* Wait till System clock is ready */
+	while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+	{
+
+	}
+	LL_Init1msTick(72000000);
+	LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+	LL_SetSystemCoreClock(72000000);
+	LL_RCC_SetUSBClockSource(LL_RCC_USB_CLKSOURCE_PLL_DIV_1_5);
 
 
-  WRITE_REG(SysTick->LOAD, 0x1193f); // 1 ms
-  LL_SYSTICK_EnableIT();
+	WRITE_REG(SysTick->LOAD, 0x1193f); // 1 ms
+	LL_SYSTICK_EnableIT();
 }
 
 /**
@@ -218,28 +218,28 @@ void SystemClock_Config(void)
   */
 static void MX_GPIO_Init(void)
 {
-  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+	LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* GPIO Ports Clock Enable */
-  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOA);
-  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOB);
+	/* GPIO Ports Clock Enable */
+	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOA);
+	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOB);
 
-  /**/
-  LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_10|LL_GPIO_PIN_11|LL_GPIO_PIN_12|LL_GPIO_PIN_13 
-                          |LL_GPIO_PIN_14|LL_GPIO_PIN_15|LL_GPIO_PIN_8|LL_GPIO_PIN_9);
+	/**/
+	LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_10|LL_GPIO_PIN_11|LL_GPIO_PIN_12|LL_GPIO_PIN_13
+	                       |LL_GPIO_PIN_14|LL_GPIO_PIN_15|LL_GPIO_PIN_8|LL_GPIO_PIN_9);
 
-  /**/
-  GPIO_InitStruct.Pin = LL_GPIO_PIN_0|LL_GPIO_PIN_1|LL_GPIO_PIN_2|LL_GPIO_PIN_3;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_FLOATING;
-  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	/**/
+	GPIO_InitStruct.Pin = LL_GPIO_PIN_0|LL_GPIO_PIN_1|LL_GPIO_PIN_2|LL_GPIO_PIN_3;
+	GPIO_InitStruct.Mode = LL_GPIO_MODE_FLOATING;
+	LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /**/
-  GPIO_InitStruct.Pin = LL_GPIO_PIN_10|LL_GPIO_PIN_11|LL_GPIO_PIN_12|LL_GPIO_PIN_13 
-                          |LL_GPIO_PIN_14|LL_GPIO_PIN_15|LL_GPIO_PIN_8|LL_GPIO_PIN_9;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+	/**/
+	GPIO_InitStruct.Pin = LL_GPIO_PIN_10|LL_GPIO_PIN_11|LL_GPIO_PIN_12|LL_GPIO_PIN_13
+	                      |LL_GPIO_PIN_14|LL_GPIO_PIN_15|LL_GPIO_PIN_8|LL_GPIO_PIN_9;
+	GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
@@ -253,47 +253,47 @@ static void MX_GPIO_Init(void)
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
 
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 static void MX_TIM2_Init(void)
 {
 	// Tick 1 us
 	// UIF every 0xFFFF * 1 us
-  /* USER CODE BEGIN TIM2_Init 0 */
+	/* USER CODE BEGIN TIM2_Init 0 */
 
-  /* USER CODE END TIM2_Init 0 */
+	/* USER CODE END TIM2_Init 0 */
 
-  LL_TIM_InitTypeDef TIM_InitStruct = {0};
+	LL_TIM_InitTypeDef TIM_InitStruct = {0};
 
-  /* Peripheral clock enable */
-  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
+	/* Peripheral clock enable */
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
 
-  /* TIM2 interrupt Init */
-  NVIC_SetPriority(TIM2_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),0, 0));
-  NVIC_EnableIRQ(TIM2_IRQn);
+	/* TIM2 interrupt Init */
+	NVIC_SetPriority(TIM2_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),0, 0));
+	NVIC_EnableIRQ(TIM2_IRQn);
 
-  /* USER CODE BEGIN TIM2_Init 1 */
+	/* USER CODE BEGIN TIM2_Init 1 */
 
-  /* USER CODE END TIM2_Init 1 */
-  TIM_InitStruct.Prescaler = 71;
-  TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
-  TIM_InitStruct.Autoreload = 0xFFFF;
-  TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
-  LL_TIM_Init(TIM2, &TIM_InitStruct);
-  LL_TIM_DisableARRPreload(TIM2);
-  LL_TIM_SetClockSource(TIM2, LL_TIM_CLOCKSOURCE_INTERNAL);
-  LL_TIM_SetTriggerOutput(TIM2, LL_TIM_TRGO_RESET);
-  LL_TIM_DisableMasterSlaveMode(TIM2);
+	/* USER CODE END TIM2_Init 1 */
+	TIM_InitStruct.Prescaler = 71;
+	TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
+	TIM_InitStruct.Autoreload = 0xFFFF;
+	TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
+	LL_TIM_Init(TIM2, &TIM_InitStruct);
+	LL_TIM_DisableARRPreload(TIM2);
+	LL_TIM_SetClockSource(TIM2, LL_TIM_CLOCKSOURCE_INTERNAL);
+	LL_TIM_SetTriggerOutput(TIM2, LL_TIM_TRGO_RESET);
+	LL_TIM_DisableMasterSlaveMode(TIM2);
 
-  LL_TIM_EnableIT_UPDATE(TIM2);
-  LL_TIM_EnableCounter(TIM2);
-  /* USER CODE BEGIN TIM2_Init 2 */
+	LL_TIM_EnableIT_UPDATE(TIM2);
+	LL_TIM_EnableCounter(TIM2);
+	/* USER CODE BEGIN TIM2_Init 2 */
 
-  /* USER CODE END TIM2_Init 2 */
+	/* USER CODE END TIM2_Init 2 */
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -305,11 +305,11 @@ static void MX_TIM2_Init(void)
   * @retval None
   */
 void assert_failed(uint8_t *file, uint32_t line)
-{ 
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+{
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+	   tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
---
+++ format.sh
@@ -0,0 +1,2 @@
+echo "http://astyle.sourceforge.net/astyle.html"
+./astyle --indent=tab --style=allman --recursive M3/Src/*.c,*.h M3/Utils/*.c,*.h
