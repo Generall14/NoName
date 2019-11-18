14638c6f0dcfc76107f0248b5dddd00576bf9d58
Wojciech Kogut <wojciech_kogut@o2.pl>
2019-11-18 22:23:42 +0100

    Some cleanup.

--- M3/Src/main.c
+++ M3/Src/main.c
@@ -1,24 +1,3 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "usb_device.h"
 #include "utils.h"
@@ -30,44 +9,9 @@
 #include "SProt/sprot_l.h"
 #include "global_clock.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
 
 int main(void)
 {
@@ -213,10 +157,6 @@ static void MX_GPIO_Init(void)
 	LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
@@ -282,5 +222,3 @@ void assert_failed(uint8_t *file, uint32_t line)
 	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- M3/UTest/fake/global_clock.c
+++ M3/UTest/fake/global_clock.c
@@ -1,11 +1,19 @@
 #include <stdint.h>
+#include "global_clock.h"
+
+uint32_t fake_global_clock_us = 0;
 
 uint32_t get_global_clock()
 {
-	return 0;
+	return fake_global_clock_us/GLOBAL_CLOCK_TO_US_MUL;
 }
 
 uint32_t get_global_clock_us()
 {
-	return 0;
+	return fake_global_clock_us;
+}
+
+uint32_t get_global_clock_ms()
+{
+	return fake_global_clock_us*GLOBAL_CLOCK_TO_MS_MUL;
 }
