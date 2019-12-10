34b3726958a7f051bd5f1bc8d3e0b0ecea560d23
Wojciech Kogut <wojciech_kogut@o2.pl>
2019-12-10 21:32:25 +0100

    Variadic arguments tests.

--- SLogTests/main.c
+++ SLogTests/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdarg.h>
 #include "tst.h"
 
 void slog_log(uint32_t log_id, ...)
 {
+	va_list args;
+    va_start(args, log_id);
+	
+	printf("slog_log: %i \n", log_id);
+	for(int i=0;i<log_id;i++)
+		printf("slog_log(%i): %i \n", i, va_arg(args, uint32_t));
 }
 
 int main()
@@ -14,5 +21,6 @@ int main()
 	printf("... %i \n", LOG_ERROR(hfg));
 	printf("... %s \n", AT);
 	printf("End\n");
+	slog_log(1, 666);
 	return 0;
 }
