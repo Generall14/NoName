7c26f990c72e8e20a58d1351d43e8b7c46588332
Wojciech Kogut <wojciech_kogut@o2.pl>
2019-12-10 21:11:30 +0100

    Some failed tests.

---
+++ SLogTests/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "tst.h"
+
+void slog_log(uint32_t log_id, ...)
+{
+}
+
+int main()
+{
+	int AA = 6;
+	//LOG_ERROR(AA);
+	printf("... %s \n", LOG_ERROR(hfg));
+	printf("... %i \n", LOG_ERROR(hfg));
+	printf("... %s \n", AT);
+	printf("End\n");
+	return 0;
+}
